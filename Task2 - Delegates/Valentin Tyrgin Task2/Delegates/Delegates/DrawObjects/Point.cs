bcbdab1fb4238019ea5d75541055eef0586bb1c4
tyrgin_notebook <valent1507@gmail.com>
2018-04-01 21:03:56 +0300

    start

---
+++ Task2 - Delegates/Valentin Tyrgin Task2/Delegates/Delegates/DrawObjects/Point.cs
@@ -0,0 +1,18 @@
+﻿namespace Delegates
+{
+    internal class Point : Board
+    {
+        public string Name { get; } = "Точка";
+
+        public override void Draw(IBoard board)
+        {
+            var utils = new Utils();
+            utils.WriteAt("x", board.GetWidth() / 4, board.GetHeight() / 4);
+        }
+
+        public override string GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Task2 - Delegates/Valentin Tyrgin Task2/Delegates/Delegates/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Utils : IUtils
+    {
+        public void WriteAt(string s, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
