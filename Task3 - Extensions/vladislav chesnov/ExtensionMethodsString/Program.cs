e108736d4a64fb50549fd36b0dbef49aedc61d94
Chesanita <chesanita@yandex.ru>
2018-04-01 11:25:44 +0300

    finished task 3

---
+++ Task3 - Extensions/vladislav chesnov/ExtensionMethodsString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionsForString;
+
+namespace ExtensionMethodsString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello world";
+            a = a.SetFirstLetterToUpperCase().ApplyBraces().ApplySpaces().AppendNumbers().IncludeCurrentYear().AppendSmile();
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task3 - Extensions/vladislav chesnov/ExtensionsForString/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExtensionsForString
+{
+    public static class Class1
+    {
+        public static string SetFirstLetterToUpperCase(this string str)
+        {
+            char[] tmp = str.ToCharArray();
+            tmp[0] = char.ToUpper(tmp[0]);
+            return string.Join("", tmp);
+        }
+
+        public static string ApplyBraces(this string str)
+        {
+            //return string.Format("{0}{1}{2}", "{", str, "}");
+            return $"{{{str}}}";
+        }
+
+        public static string ApplySpaces(this string str)
+        {
+            return $"{{{string.Join(" ", str.Split('{', '}'))}}}";
+        }
+
+        public static string AppendSmile(this string str)
+        {
+            return $":){str}";
+        }
+
+        public static string AppendNumbers(this string str)
+        {
+            return $"{str} - 42";
+        }
+
+        public static string IncludeCurrentYear(this string str)
+        {
+            int currentYear = DateTime.Now.Year;
+            return $"{str}: {currentYear}";
+        }
+    }
+}
