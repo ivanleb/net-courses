c2a04675fee5895a0923b0339b30c4388a9efa7b
Denis Chesnokov <denis_chesnokov@epam.com>
2018-04-02 14:39:49 +0300

    included examples of terms covariant, contrvariant

---
+++ Task2 - Delegates/netExample2CovContr/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace netExample2
+{
+    class Car
+    {
+        public string Name { get; set; }
+        public Car(string name)
+        {
+            Name = name;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class BMW : Car
+    {
+        public BMW(string name) : base(name)
+        {
+
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I'm BMW :) -> " + Name);            
+        }
+    }
+
+    delegate Car CarFactory(string name);
+
+    delegate void PrintBMWInfo(BMW client);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarFactory buildCar = BuildCar; // covariant assign
+            Car bmwX5 = buildCar("BMW X5");
+            bmwX5.Display();
+
+            PrintBMWInfo getCarInfo = GetCarInfo; // contrvariant assign
+            getCarInfo((BMW)bmwX5);
+        }
+
+        private static BMW BuildCar(string name)
+        {
+            return new BMW(name);
+        }
+
+        private static void GetCarInfo(Car p){
+            p.Display();
+        }
+    }
+
+   
+}
---
+++ Task2 - Delegates/netExample3GenericCovContr/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace netExample3
+{
+     class Car
+    {
+        public string Name { get; set; }
+        public Car(string name)
+        {
+            Name = name;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class BMW : Car
+    {
+        public BMW(string name) : base(name)
+        {
+
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I'm BMW :) -> " + Name);            
+        }
+    }
+
+    class Program
+    {
+        delegate T Builder<out T>(string name); // out is important for covariant
+        delegate void GetInfo<in T>(T item); // in is important for covariant
+
+        static void Main(string[] args)
+        {
+            Builder<Car> carBuilder = BuildCar;
+            Builder<BMW> bmwBuilder = BuildBMW;
+
+            carBuilder = bmwBuilder; // covariant
+
+            Car bmwX11 = carBuilder("X11");
+            bmwX11.Display();
+
+            GetInfo<BMW> bmwInfo = BmwInfo;
+            GetInfo<Car> carInfo = CarInfo;
+
+            bmwInfo = carInfo; // contravariant
+
+            carInfo(bmwX11);
+        }
+
+        private static void BmwInfo(BMW bmw)
+        {
+            bmw.Display();
+        }
+ 
+        private static void CarInfo(Car car)
+        {
+            car.Display();
+        }
+
+        private static Car BuildCar(string name)
+        {
+            return new Car(name);
+        }
+        private static BMW BuildBMW(string name)
+        {
+            return new BMW(name);
+        }
+    }
+}
