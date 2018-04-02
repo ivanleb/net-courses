2d3029084574ce0b8ee411204ae4eb696afa5c51
Vladimir Bogdan <bogdanv@mail.ru>
2018-04-02 20:19:36 +0300

    Is not ready

--- Task4 - Partials/Vladimir_Bogdan/T4Template/T4Example/T4ExampleConsoleApp/Program.cs
+++ Task4 - Partials/Vladimir_Bogdan/T4Template/T4Example/T4ExampleConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataFile;
 
 namespace T4ExampleConsoleApp
 {
@@ -10,6 +11,16 @@ namespace T4ExampleConsoleApp
     {
         static void Main(string[] args)
         {
+            Catalog myCatalog = new Catalog();
+            List<Artist> Artists = new List<Artist>(2);
+            Artists[0] = new Artist()
+            {
+                Name = "Armin van Buuren",
+                Id = "0",
+                Comments = "",
+                BirthdayDate = new DateTime(year: 1976, month: 12, day: 25),
+                Songs = new List<Song>(2) { new Song() { }, new Song() { } }
+            };
         }
     }
 }
---
+++ Task4 - Partials/Vladimir_Bogdan/T4Template/T4Example/T4ExampleCore/Partials/Artist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFile
+{
+    public partial class Artist
+    {
+        public string Comments { get; set; }
+        public int Age
+        {
+            get
+            {
+                var today = DateTime.Now.Date;
+                return today.Year - this.BirthdayDate.Year - 1 + (today.DayOfYear >= this.BirthdayDate.DayOfYear ? 1 : 0);
+            }
+        }
+        public DateTime BirthdayDate { get; set; }
+    }
+}
