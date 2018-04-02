c59b38d86db0d7b95fdd8cc4e631661f35037e11
Третьяков Александр Витальевич <backsapce@hotmail.com>
2018-04-02 16:29:58 +0300

    Done task 3.

---
+++ Task3 - Extensions/Tretiakov_Aleksandr/StringExtensionsApp/StringExtensionsApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExtensionMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input your sample:");
+                var sample = Console.ReadLine();
+                Console.WriteLine($"sample: {sample}");
+                var result = sample
+                    .SetFirstLetterToUpperCase()
+                    .ApplyBraces()
+                    .ApplySpaces()
+                    .AppendNumbers()
+                    .IncludeCurrentYear()
+                    .AppendSmile();
+                Console.WriteLine($"result: {result}");
+                Console.ReadLine();
+            } 
+        }
+    }
+}
