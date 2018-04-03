a04bdfb37b3984a9759bd53607fef5707b83422a
tyrgin_notebook <valent1507@gmail.com>
2018-04-03 11:46:17 +0300

    Ready for review

---
+++ Task4 - Partials/Valentin Tyrgin Task4/T4/T4/Core/Partials/Artist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace T4
+{
+    partial class Artist
+    {
+        public string Comments { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthdayDate { get; set; }
+     }
+}
---
+++ Task4 - Partials/Valentin Tyrgin Task4/T4/T4/Core/Partials/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace T4
+{
+    partial class Book
+    {
+        public string Format { get; set; }
+        public string Author { get; set; }
+        public int PagesAmount { get; set; }
+    }
+}
