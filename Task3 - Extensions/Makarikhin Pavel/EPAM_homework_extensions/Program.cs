fab517e1a707937b7819ccb95d500c620125858a
CunningPie <Makarikhin.P.A@gmail.com>
2018-04-03 21:32:19 +0300

    task 3 extensions

---
+++ Task3 - Extensions/Makarikhin Pavel/EPAM_homework_extensions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_homework_extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "simple text";
+
+            var result = a.SetFirstLetterToUpperCase().AppendNumber(248).AppendSmile().ApplyBraces().IncludeCurrentTime();
+
+            Console.WriteLine(a);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3 - Extensions/Makarikhin Pavel/EPAM_homework_extensions/StringExtensionMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_homework_extensions
+{
+    public static class StringExtensionMethods
+    {
+        public static string SetFirstLetterToUpperCase(this string str)
+        {
+            return char.ToUpper(str[0]) + (str.Substring(1));
+        }
+
+        public static string AppendSmile(this string str)
+        {
+            return ":)" + str;
+        }
+
+        public static string ApplyBraces(this string str)
+        {
+            return "{ " + str + " }";
+        }
+
+        public static string ApplySpaces(this string str)
+        {
+            return " " + str + " ";
+        }
+        public static string AppendNumber(this string str, int num)
+        {
+            return str + ": " + num;
+        }
+
+        public static string IncludeCurrentTime(this string str)
+        {
+            return str + " " + System.DateTime.Now.ToString();
+        }
+
+    }
+}
