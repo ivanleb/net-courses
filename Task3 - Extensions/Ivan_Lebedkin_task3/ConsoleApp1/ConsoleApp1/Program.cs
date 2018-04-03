f3722839b1da26a1c5c3d9c19fbbc62c6cf0964f
ivanleb <ivan_leb@mail.ru>
2018-04-03 22:12:19 +0300

    Extensions project was created

---
+++ Task3 - Extensions/Ivan_Lebedkin_task3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String sampleStr = "sample texts";
+            var a = sampleStr
+                .SetFirstLetterToUpperCase()
+                .ApplyBraces()
+                .ApplySpaces()
+                .AppendNumbers()
+                .IncludeCurrentYear()
+                .AppendSmile();
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Task3 - Extensions/Ivan_Lebedkin_task3/ConsoleApp1/ConsoleApp1/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class StringExtensions
+    {
+        public static String SetFirstLetterToUpperCase(this String str)
+        {
+            return new StringBuilder(str).Replace(str[0], char.ToUpper(str[0]), 0, 1).ToString();
+        }
+
+        public static String ApplyBraces(this String str)
+        {
+            return new StringBuilder(str).Insert(0, "{").Insert(str.Length+1 , "}").ToString();
+        }
+        public static String ApplySpaces(this String str)
+        {
+            return new StringBuilder(str).Insert(0, " ").Insert(str.Length + 1, " ").ToString();
+        }
+        public static String AppendNumbers(this String str)
+        {
+            return new StringBuilder(str).Append(" - 12345").ToString(); //  - 12345
+        }
+        public static String IncludeCurrentYear(this String str)
+        {
+            return new StringBuilder(str).Append(" :" +DateTime.Now.Year).ToString();
+        }
+        public static String AppendSmile(this String str)
+        {
+            return new StringBuilder(str).Insert(0,")").Insert(0,':').ToString();
+        }
+
+    }
+}
