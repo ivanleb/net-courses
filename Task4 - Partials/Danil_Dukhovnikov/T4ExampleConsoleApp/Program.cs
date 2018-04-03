6572784f6558d42288aefea8a7c78a7a7271eaad
Danil <DukhovnikovDanil@yandex.ru>
2018-04-03 19:13:42 +0300

    Little fix.

--- Task4 - Partials/Danil_Dukhovnikov/T4ExampleConsoleApp/Program.cs
+++ Task4 - Partials/Danil_Dukhovnikov/T4ExampleConsoleApp/Program.cs
@@ -7,74 +7,82 @@ using MyNameSpace;
 
 namespace T4ExampleConsoleApp
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-#region Creating
-            Song fisrt = new Song();
-            fisrt.Id = "21";
-            Song second = new Song();
-            second.Id = "22";
-            Song third = new Song();
-            third.Id = "23";
-            Song fourth = new Song();
-            fourth.Id = "24";
-            Song fifth = new Song();
-            fifth.Id = "25";
-            Song sixth = new Song();
-            sixth.Id = "26";
+            #region Creating
 
-            Artist rhcp = new Artist();
-            rhcp.Age = 55;
-            rhcp.BirthdayDate = new DateTime(1952, 11, 1);
-            rhcp.Comments = "Lead singer of Red Hot Chilly Peppers band";
-            rhcp.Id = "1";
-            rhcp.Name = "Anthony Kiedis";
-            rhcp.Songs = new List<Song> { fisrt, second, third };
+            var fisrt = new Song {Id = "21"};
+            var second = new Song {Id = "22"};
+            var third = new Song {Id = "23"};
+            var fourth = new Song {Id = "24"};
+            var fifth = new Song {Id = "25"};
+            var sixth = new Song {Id = "26"};
 
-            Artist ratm = new Artist();
-            ratm.Age = 48;
-            ratm.BirthdayDate = new DateTime(1970, 1, 12);
-            ratm.Comments = "Lead singer of Rage Against the Machine band";
-            ratm.Id = "2";
-            ratm.Name = "Zak de la Rocha";
-            ratm.Songs = new List<Song> { fourth, fifth };
+            var rhcp = new Artist
+            {
+                Age = 55,
+                BirthdayDate = new DateTime(1952, 11, 1),
+                Comments = "Lead singer of Red Hot Chilly Peppers band",
+                Id = "1",
+                Name = "Anthony Kiedis",
+                Songs = new List<Song> {fisrt, second, third}
+            };
 
-            Artist splean  = new Artist();
-            splean.Age = 48;
-            splean.BirthdayDate = new DateTime(1969, 07, 15);
-            splean.Comments = "Lead singer and leader of Russian band Splean";
-            splean.Id = "3";
-            splean.Name = "Vasilyev Alexander Georgievich";
-            splean.Songs = new List<Song> { sixth };
+            var ratm = new Artist
+            {
+                Age = 48,
+                BirthdayDate = new DateTime(1970, 1, 12),
+                Comments = "Lead singer of Rage Against the Machine band",
+                Id = "2",
+                Name = "Zak de la Rocha",
+                Songs = new List<Song> {fourth, fifth}
+            };
 
-            Book warAndPeace = new Book();
-            warAndPeace.Author = "Leo Tolstoy";
-            warAndPeace.Format = "Paper";
-            warAndPeace.Id = "4";
-            warAndPeace.Name = "War and Peace";
-            warAndPeace.OneProperty = "8 out of 10";
-            warAndPeace.PagesAmount = 1225;
+            var splean = new Artist
+            {
+                Age = 48,
+                BirthdayDate = new DateTime(1969, 07, 15),
+                Comments = "Lead singer and leader of Russian band Splean",
+                Id = "3",
+                Name = "Vasilyev Alexander Georgievich",
+                Songs = new List<Song> {sixth}
+            };
 
-            Book deadSouls = new Book();
-            deadSouls.Author = "Nikolai Gogol";
-            deadSouls.Format = "Paper";
-            deadSouls.Id = "5";
-            deadSouls.Name = "Dead Souls";
-            deadSouls.OneProperty = "9 out of 10";
-            deadSouls.PagesAmount = 410;
-#endregion
+            var warAndPeace = new Book
+            {
+                Author = "Leo Tolstoy",
+                Format = "Paper",
+                Id = "4",
+                Name = "War and Peace",
+                OneProperty = "8 out of 10",
+                PagesAmount = 1225
+            };
 
-            Catalog catalog = new Catalog();
-            catalog.Artists = new List<Artist> { rhcp, ratm, splean };
-            catalog.Books = new List<Book> { warAndPeace, deadSouls };
+            var deadSouls = new Book
+            {
+                Author = "Nikolai Gogol",
+                Format = "Paper",
+                Id = "5",
+                Name = "Dead Souls",
+                OneProperty = "9 out of 10",
+                PagesAmount = 410
+            };
 
-            foreach(Artist a in catalog.Artists)
+            #endregion
+
+            var catalog = new Catalog
+            {
+                Artists = new List<Artist> {rhcp, ratm, splean},
+                Books = new List<Book> {warAndPeace, deadSouls}
+            };
+
+            foreach(var a in catalog.Artists)
             {
                 Console.WriteLine($"{a.Name} -  {a.Comments}, date of birth - {a.BirthdayDate.ToShortDateString()} " +
-                    $"now {a.Age} years old, number of songs in database {a.Songs.Count()}, id in database - {a.Id}" +
-                    $"\nId of songs:");
+                                  $"now {a.Age} years old, number of songs in database {a.Songs.Count()}, id in database - {a.Id}" +
+                                  $"\nId of songs:");
                 foreach(Song s in a.Songs)
                 {
                     Console.Write($"|{s.Id}| ");
@@ -83,10 +91,10 @@ namespace T4ExampleConsoleApp
                 
             }
 
-            foreach(Book b in catalog.Books)
+            foreach(var b in catalog.Books)
             {
                 Console.WriteLine($"\nBook name - {b.Name}, author - {b.Author}, format - {b.Format}, " +
-                    $"number of pages = {b.PagesAmount}, rating in database - {b.OneProperty}, id in database - {b.Id}");
+                                  $"number of pages = {b.PagesAmount}, rating in database - {b.OneProperty}, id in database - {b.Id}");
             }
             Console.ReadLine();
 
