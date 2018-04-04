e252e630cce05bb4b79244e85291394349e3d502
Anna Zakharchenko <anna_vzahar@mail.ru>
2018-04-04 18:44:01 +0300

    task 3 anna

---
+++ Task3 - Extensions/Anna Zakharchenko/StringExtantion/StringExtantion/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "simple text";
+            var result = a
+                .SetFirstLetterToUpperCase()
+                .ApplySpaces()
+                .ApplyBraces()
+                .AppendNumbers(12345)
+                .IncludeCurrentYear()
+                .AppendSmile();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static String SetFirstLetterToUpperCase(this String str)
+        {
+            char[] inCharacter = str.ToCharArray();
+            inCharacter[0] = char.ToUpper(inCharacter[0]);
+            return str;
+        }
+
+        public static String ApplyBraces(this String str)
+        {
+            return "{" + str + "}";
+        }
+
+        public static String ApplySpaces(this String str)
+        {
+            return " " + str + " ";
+        }
+
+        public static String AppendNumbers(this String str, int number)
+        {
+            return str + " - " + number;
+        }
+
+        public static String IncludeCurrentYear(this String str)
+        {
+            return str + ": " + DateTime.Today.Year;
+        }
+
+        public static String AppendSmile(this String str)
+        {
+            return ":)" + str;
+        }
+    }
+}
