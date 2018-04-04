3e11d03a22b592417e5e8f95c63fdb323a122f7e
ivanleb <ivan_leb@mail.ru>
2018-04-04 15:01:32 +0300

    task4 ready for review

---
+++ Task4 - Partials/Ivan_Lebedkin_task4/T4Template/T4Example/T4ExampleConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataFile;
+//using T4ExampleCore;
+
+namespace T4ExampleConsoleApp
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Artist artist = new Artist();           
+            Book book = new Book();
+            Song sng = new Song();
+
+            Catalog catalog = new Catalog();
+            
+            List<Artist> artst = new List<Artist>();
+            artst.Add(artist);
+            List<Book> bks = new List<Book>();
+            bks.Add(book);
+
+            catalog.Artists = artst;
+            catalog.Books = bks;
+
+            catalog.Artists.All(x => { Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}", x.Name, x.BirthdayDate, x.Age, x.Id, x.Comments);
+                x.Songs.All(y => { Console.WriteLine(y.Id); return true; });
+                return true; });
+            Console.WriteLine();
+            catalog.Books.All(x => 
+            {
+                Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}", x.Name, x.Author, x.PagesAmount, x.Id, x.OneProperty, x.Format);
+                return true;
+            });
+        }
+    }
+}
---
+++ Task4 - Partials/Ivan_Lebedkin_task4/T4Template/T4Example/T4ExampleCore/Artist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFile
+{
+    public partial class Artist
+    {
+        public Artist()
+        {
+            this.Age = 53;
+            this.BirthdayDate = new DateTime(1964, 07, 27);
+            this.Id = "01";
+            this.Name = "Jeffrey Richter";
+            this.Comments = "microsoft";
+            List<Song> defaultSong = new List<Song>();
+            Song song = new Song();
+            song.Id = "CLR via C#";
+            defaultSong.Add(song);
+            this.Songs = defaultSong;
+        }
+        public String Comments { get; set; }
+        public Int32 Age { get; set; }
+        public DateTime BirthdayDate { get; set; }
+    }
+}
