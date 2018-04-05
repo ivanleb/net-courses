c0382a275f12bf27edb0303158599d46f36b94d6
Simon Kutuzov <computer016@gmail.com>
2018-04-05 15:27:47 +0300

    Task 4 done.

---
+++ Task4 - Partials/Simon_Kutuzov/T4Example/T4ExampleCore/Partials/Artist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DataFile
+{
+    public partial class Artist
+    {
+        public string Comments { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthdayDate { get; set; }
+    }
+}
---
+++ Task4 - Partials/Simon_Kutuzov/T4Example/T4ExampleCore/Partials/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace DataFile
+{
+    public partial class Book
+    {
+        public string Format { get; set; }
+        public string Author { get; set; }
+        public int PagesAmount { get; set; }
+    }
+}
