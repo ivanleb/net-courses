b4559afef1caadb0bddd9021d4c63bb049a2c9c2
Simon Kutuzov <computer016@gmail.com>
2018-04-05 13:26:53 +0300

    Task 3 done

---
+++ Task3 - Extensions/Simon_Kutuzov/Extensions/Extensions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "simple text";
+            string result = s
+                .SetFirstLetterToUpperCase()
+                .ApplySpaces()
+                .ApplyBraces()
+                .AppendNumbers()
+                .IncludeCurrentYear()
+                .AppendSmile();
+
+            Console.WriteLine(result);
+        }
+    }
+}
