750780601a05021cd8bc691afa155dc74bf2e874
KRTone <ruslank398@gmail.com>
2018-04-06 15:46:26 +0300

    IsUseFilter Added

--- Task6 - Lambda/RuslanKudaybergenovTask6/RuslanKudaybergenovTask6/Program.cs
+++ Task6 - Lambda/RuslanKudaybergenovTask6/RuslanKudaybergenovTask6/Program.cs
@@ -9,17 +9,29 @@ namespace RuslanKudaybergenovTask6
 {
     class NumberGenerator
     {
+        bool IsUseFilter(Action<int> onNumberRecieved, IEnumerable<Func<int, bool>> useFilter)
+        {
+            foreach(var filter in useFilter)
+            {
+                for (int i = 0; i < 101; i++)
+                    if (!filter(i))
+                        return false;
+            }
+            return true;
+        }
         public void Subscribe(Action<int> onNumberReceived, IEnumerable<Func<int, bool>> useFilter)
         {
-            foreach (var filter in useFilter)
-            {
-                Console.WriteLine(filter.Method.Name);
-                for (int i = 0; i < 101; i++)
-                    if (filter(i))
-                    {
-                        onNumberReceived(i);
-                    }
-            }
+
+            if (IsUseFilter(onNumberReceived, useFilter))
+                foreach (var filter in useFilter)
+                {
+                    Console.WriteLine(filter.Method.Name);
+                    for (int i = 0; i < 101; i++)
+                        if (filter(i))
+                        {
+                            onNumberReceived(i);
+                        }
+                }
         }
 
         public bool Filter1(int x)
@@ -33,6 +45,10 @@ namespace RuslanKudaybergenovTask6
         public bool Filter3(int x)
         {
             return x % 2 != 0 && x % 3 != 0 && x % 5 != 0;
+        }
+        public bool Filter4(int x)
+        {
+            return x % 1 == 0;
         }
         public void GetNumber( int i)
         {
@@ -53,7 +69,13 @@ namespace RuslanKudaybergenovTask6
                 ng.Fileter2,
                 ng.Filter3
             };
+            List<Func<int, bool>> client2FuncList = new List<Func<int, bool>>()
+            {
+                ng.Filter4
+            };
+
             ng.Subscribe(ng.GetNumber, client1FuncList);
+            ng.Subscribe(ng.GetNumber, client2FuncList);
             Console.ReadKey();
         }
     }
