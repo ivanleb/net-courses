dede56d2e6db04b55ada01fc42bbd414c9a9cfb9
BogdanVU <bogdanv@mail.ru>
2018-04-08 19:31:46 +0300

    Task 6 is ready for checking.

---
+++ Task6 - Lambda/Vladimir_Bogdan/Lambdas/Lambdas/NumberGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lambdas
+{
+    class NumberGenerator
+    {
+        private class Subscriber
+        {
+            private Action<int> onNumberReceived;
+            private IEnumerable<Func<int, bool>> useFilters;
+            public Subscriber(Action<int> OnNumberReceived, IEnumerable<Func<int, bool>> UseFilters)
+            {
+                this.onNumberReceived = OnNumberReceived;
+                this.useFilters = UseFilters;
+            }
+            private bool SatisfiesTheFilters (int number)
+            {
+                if (this.useFilters == null) return true;
+                foreach (var filterItem in useFilters)
+                {
+                    if (!filterItem(number)) return false;
+                }
+                return true;
+            }
+            public void GetNotification (int number)
+            {
+                if ((onNumberReceived != null) && (SatisfiesTheFilters(number)))
+                {
+                    onNumberReceived(number);
+                }
+            }
+        }
+        private List<Subscriber> Subscribers = new List<Subscriber>();
+        private delegate void Notification(int number);
+        Notification Notify = null;
+        public void Subscribe(Action<int> onNumberReceived, IEnumerable<Func<int, bool>> useFilter)
+        {
+            var subscriber = new Subscriber(onNumberReceived, useFilter);
+            Subscribers.Add(subscriber);
+            Notify += subscriber.GetNotification;
+        }
+        public void Generate(int count)
+        {
+            for (int i=0; i<count; i++)
+            {
+                Notify(i);
+            }
+        }
+    }
+}
---
+++ Task6 - Lambda/Vladimir_Bogdan/Lambdas/Lambdas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberGenerator ng = new NumberGenerator();
+            ng.Subscribe(x => Console.WriteLine("Client1 got " + x), null);
+            var filter = new List<Func<int, bool>>() { (x) => { return x % 3 == 0; }, (x) => { return x % 5 == 0; } };
+            ng.Subscribe(x=>Console.WriteLine("Client2: This number is a multiple of 15. That's why i got "+x), filter);
+            ng.Generate(20);
+            Console.ReadKey(true);
+        }
+    }
+}
