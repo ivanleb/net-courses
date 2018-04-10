ac2cdbf7a9888f8d5c878096b6cd5ffe58de8786
tatiana chukanova <sunstroke.chu@gmail.com>
2018-04-11 00:38:16 +0300

    task6

---
+++ Task6 - Lambda/Tatiana Klimenko/tatiana-klimenko-task6/tatiana-klimenko-task6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tatiana_klimenko_task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberGenerator numGen = new NumberGenerator(100);
+
+            Client client1 = new Client("client 1");
+            Client client2 = new Client("client 2");
+
+            //subscribe with one filter
+            numGen.Subscribe((x) =>
+            {
+                client1.HandleOutput(x);
+            }, 
+            (x) => 
+            {
+                return x % 8 == 0;
+            });
+
+            //subscribe with several filters
+            numGen.Subscribe((x) =>
+            {
+                client2.HandleOutput(x);
+            },
+            new List<Func<int,bool>>
+            {
+                (x) => { return x * x < 1000; },
+                (x) => { return x % 10 == 0; }
+            });
+
+
+        }
+    }
+}
