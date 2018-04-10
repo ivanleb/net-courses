8fb3a67643d918435d430bcf667350a4c275c286
Alena Zhigalova <alenazhigalova@mail.ru>
2018-04-11 02:13:05 +0300

    create-task4

---
+++ Task4 - Partials/Alena Zhigalova Task4/T4Example/T4ExampleCore/Partials/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4Example
+{
+    public partial class Artist
+    {
+        public string Comments { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthdayDate { get; set; }
+    }
+}
