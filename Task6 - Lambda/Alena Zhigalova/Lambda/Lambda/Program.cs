6e516830b6eaf32e38b652198ef58d1589f4e9ae
Alena Zhigalova <alenazhigalova@mail.ru>
2018-04-11 03:30:47 +0300

    create task6

---
+++ Task6 - Lambda/Alena Zhigalova/Lambda/Lambda/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        class NumberGenerator
+        {
+           public void Subscribe (Action<int> onNumberReceived, Func<int, bool> useFilter)
+            {
+                for ( int i = 0; i < 150;i++)
+                {
+                    if (useFilter(i)) onNumberReceived(i);
+                }
+            }
+            public void GetNumber(int i)
+            {
+                Console.WriteLine("Good number: " + i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            NumberGenerator generator = new NumberGenerator();
+            Console.WriteLine("Every 10th number:");
+            generator.Subscribe((x) =>
+            {
+                // should be even only
+                generator.GetNumber(x);
+
+            }, (x) =>
+            {
+                return x % 10 == 0;// declare rule for even.
+            });
+            Console.WriteLine("Every > 99 and %5 number:");
+            generator.Subscribe((x) =>
+            {
+                // should be even only
+                generator.GetNumber(x);
+
+            }, (x) =>
+            {
+                return ((x % 5 == 0)&(x>99));// declare rule for even.
+            });
+            Console.ReadKey();
+        }
+    }
+}
