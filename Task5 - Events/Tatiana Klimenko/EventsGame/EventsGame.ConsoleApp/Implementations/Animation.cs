c3b74ae905d3eb2f90456a51c49ea2da1c87f592
tatiana chukanova <sunstroke.chu@gmail.com>
2018-04-12 23:31:27 +0300

    task 5

---
+++ Task5 - Events/Tatiana Klimenko/EventsGame/EventsGame.ConsoleApp/Implementations/Animation.cs
@@ -0,0 +1,8 @@
+﻿using EventsGame.Core.Abstractions;
+
+namespace EventsGame.ConsoleApp.Implementations
+{
+    class Animation : IAnimation
+    {
+    }
+}
---
+++ Task5 - Events/Tatiana Klimenko/EventsGame/EventsGame.ConsoleApp/Implementations/Registry.cs
@@ -0,0 +1,24 @@
+﻿using EventsGame.Core.Abstractions;
+
+namespace EventsGame.ConsoleApp.Implementations
+{
+    class Registry : IRegistry
+    {
+        public IBoard Board { get; set; }
+        public IHero Hero { get; set; }
+        public IDrawing Drawing { get; set; }
+        public IAnimation Animation { get; set; }
+        public IUserInteraction UserInteraction { get; set; }
+        public IGameEventArgs GetGameEventArgs { get; set; }
+
+        public Registry()
+        {
+            this.Board = new Board();
+            this.Hero = new Hero();
+            this.Drawing = new Drawing();
+            this.Animation = new Animation();
+            this.UserInteraction = new UserInteraction();
+            this.GetGameEventArgs = new GameEventArgs();
+        }
+    }
+}
