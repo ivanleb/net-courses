37f76cd05013ef7096a5f2194fd340e1ea5c6791
Nikita Nuttunen <nikitanut@gmail.com>
2018-04-16 22:58:55 +0300

    Done task7

---
+++ Task7 - LinqTo/Nikita_Nuttunen/Linq/LinqCore/Entities/Device.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqCore.Entities
+{
+    public class Device
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public override string ToString()
+        {
+            string available = IsAvailable ? "Available" : "Not available";
+            
+            return $"{Id, 2} | {Name, 25} | {Price, 5} | {Category, 11} | {available}";
+        }
+    }
+}
---
+++ Task7 - LinqTo/Nikita_Nuttunen/Linq/LinqCore/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqCore.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Total { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id, 2} | CustomerId: {CustomerId, 2} | Date: {Date.ToShortDateString(), 10} | {Total}";
+        }
+    }
+}
