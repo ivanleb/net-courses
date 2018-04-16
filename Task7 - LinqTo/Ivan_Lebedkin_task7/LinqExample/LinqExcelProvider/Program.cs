fe369dfdfb361dee8cc2acafe262c8590dfc1c16
ivanleb <ivan_leb@mail.ru>
2018-04-16 17:58:44 +0300

    task7

---
+++ Task7 - LinqTo/Ivan_Lebedkin_task7/LinqExample/LinqExcelProvider/Program.cs
@@ -0,0 +1,56 @@
+﻿using LinqExampleCore;
+using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExcelProvider
+{
+    class ExcelLinqDataModel : IDataModel
+    {
+        private readonly ExcelQueryFactory dataProvider;
+
+        public ExcelLinqDataModel(string pathToDataFile)
+        {
+            this.dataProvider = new ExcelQueryFactory(pathToDataFile);
+        }
+
+        public IQueryable<Book> Books
+        {
+            get
+            {
+                return dataProvider.Worksheet<Book>();
+            }
+        }
+
+        public IQueryable<Dinosaur> Dinosaurs
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IQueryable<HistoricalFigure> HistoricalFigures
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            IDataModel dataModel = new ExcelLinqDataModel(".\\data.xlsx");
+
+            dataModel.ShowOutput();
+        }
+    }
+}
