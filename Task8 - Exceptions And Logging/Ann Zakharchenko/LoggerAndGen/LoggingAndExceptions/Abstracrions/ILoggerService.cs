a543dcd045bf11935becbe93c2fba01363070a03
Anna Zakharchenko <anna_vzahar@mail.ru>
2018-04-26 21:38:49 +0300

    first level

---
+++ Task8 - Exceptions And Logging/Ann Zakharchenko/LoggerAndGen/LoggingAndExceptions/Abstracrions/ILoggerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingAndExceptions.Abstracrions
+{
+    public interface ILoggerService
+    {
+        void RunWithExceptionLogging(Action actionToRun, bool isSilent = false);
+        T RunWithExceptionLogging<T>(Func<T> funcToRun, bool isSilent = false);
+        void Info(string message);
+        void Error(Exception ex);
+    }
+}
---
+++ Task8 - Exceptions And Logging/Ann Zakharchenko/LoggerAndGen/LoggingAndExceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoggingAndExceptions.Implementation;
+using log4net;
+using log4net.Config;
+
+namespace LoggingAndExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            ILog logger = LogManager.GetLogger("TextLogger");
+            LoggerService loggerService = new LoggerService(logger);
+            FirstProducer firstProducer = new FirstProducer(loggerService);
+            SecondProducer secondProducer = new SecondProducer(loggerService);
+
+            Task.Run(() =>
+            {
+                firstProducer.Run((point) => loggerService.Info($"First Function {point}"));
+            });
+
+            Task.Run(() => 
+            {
+                secondProducer.Run((point) => loggerService.Info($"Second Function {point}"));
+            });
+
+            Console.ReadLine();
+
+            secondProducer.IsContinue = false;
+            firstProducer.IsContinue = false;
+
+            Console.WriteLine("Good Bay!");
+            Console.ReadKey();
+        }
+    }
+}
