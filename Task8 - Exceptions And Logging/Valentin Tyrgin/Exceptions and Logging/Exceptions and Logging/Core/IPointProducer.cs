03c0638a7e3bf3d2e475ee2d38202ccae6644267
tyrgin_notebook <valent1507@gmail.com>
2018-05-04 09:09:17 +0300

    Valentin Tyrgin Task8

---
+++ Task8 - Exceptions And Logging/Valentin Tyrgin/Exceptions and Logging/Exceptions and Logging/Core/IPointProducer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions_and_Logging.Core
+{
+    interface IPointProducer
+    {
+        bool IsContinue { get; set; }
+        event EventHandler<IPoint> OnPointProduced;
+        void Run(Action<IPoint> onPointAction);
+    }
+}
---
+++ Task8 - Exceptions And Logging/Valentin Tyrgin/Exceptions and Logging/Exceptions and Logging/Core/Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace Exceptions_and_Logging.Core
+{
+    public static class Logger
+    {
+        public static ILog Log { get; } = LogManager.GetLogger("LOGGER");
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        public static void RunWithExceptionLogging(Action actionToRun, bool isSilent = false)
+        {
+            try
+            {
+                actionToRun();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+
+                if (isSilent)
+                {
+                    return;
+                }
+
+                throw;
+            }
+        }
+
+        public static T RunWithExceptionLogging<T>(Func<T> functionToRun, bool isSilent = false)
+        {
+            try
+            {
+                return functionToRun();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+
+                if (isSilent)
+                {
+                    return default(T);
+                }
+
+                throw;
+            }
+        }
+    }
+}
