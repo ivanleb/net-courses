2b9e02dfcaf0d190d887fc96340c5a8a6926230d
Anna Zakharchenko <anna_vzahar@mail.ru>
2018-05-26 14:48:05 +0300

    without changes

---
+++ Task10 - DI and UnitTests/Anna Zakharchenko/EntityCodeFirstTradesWithTestsAndContaner/EntityConsoleApp/BaseDbContext.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using EntityCore.Abstractions;
+using EntityCore.Model;
+using System.Data.Entity;
+
+namespace EntityConsoleApp
+{
+    public abstract class BaseDbContext : DbContext, IDataContext
+    {
+        protected BaseDbContext(string connectionString) : base(connectionString)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Trade> Trades { get; set; }
+        IQueryable<Client> IDataContext.Clients => this.Clients;
+
+        IQueryable<Stock> IDataContext.Stocks => this.Stocks;
+
+        IQueryable<Trade> IDataContext.Trades => this.Trades;
+
+        public void Add(Client entity) => Clients.Add(entity);
+        public void Remove(Client entity) => Clients.Remove(entity);
+        public void Update(Client entity)
+        {
+            Client modified = Clients.First(f => f.Id == entity.Id);
+            modified.FirstName = entity.FirstName;
+            modified.LastName = entity.LastName;
+            modified.PhoneNumber = entity.PhoneNumber;
+            modified.Balance = entity.Balance;
+        }
+
+        public void Add(Stock entity) => Stocks.Add(entity);
+        public void Remove(Stock entity) => Stocks.Remove(entity);
+        public void Update(Stock entity)
+        {
+            Stock modified = Stocks.First(f => f.Id == entity.Id);
+            modified.TypeOfStock = entity.TypeOfStock;
+        }
+
+        public void Add(Trade entity) => Trades.Add(entity);
+        public void Remove(Trade entity) => Trades.Remove(entity);
+        public void Update(Trade entity)
+        {
+            Trade modified = Trades.First(f => f.Id == entity.Id);
+            modified.Seller = entity.Seller;
+            modified.Buyer = entity.Buyer;
+            modified.StockFromSeller = entity.StockFromSeller;
+        }
+
+        void IDataContext.SaveChanges() => SaveChanges();
+    }
+}
---
+++ Task10 - DI and UnitTests/Anna Zakharchenko/EntityCodeFirstTradesWithTestsAndContaner/EntityConsoleApp/Implementations/GoodTradeProducer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityCore;
+using EntityCore.Model;
+
+namespace EntityConsoleApp.Implementations
+{
+    class GoodTradeProducer : TradeProducer
+    {
+        public event EventHandler<Trade> OnBalanceChanged;
+        public GoodTradeProducer(LoggerService loggerService, BussinesService bussinesService) : base(loggerService, bussinesService)
+        {
+        }
+        public override void MakeTrade(Client seller, Client buyer, Stock stockForSale)
+        {
+            Trade trade = bussinesService.GetNewTrade(seller, buyer, stockForSale);
+            bussinesService.RegisterNewTrade(trade);
+            OnBalanceChanged?.Invoke(this, trade);
+        }
+    }
+}
